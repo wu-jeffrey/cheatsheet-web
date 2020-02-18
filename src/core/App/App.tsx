9b2f0aed5f4ebe8bb89e5bd325847d4978c76788
Mark Sim <mark.sim@shopify.com>
2020-02-17 19:51:13 -0500

    added react router and few routes

--- src/core/App/App.tsx
+++ src/core/App/App.tsx
@@ -1,14 +1,20 @@
 import React from "react";
 import "./App.css";
-// import Home from '../../sections/Home/Home'
+import {Route, Switch, BrowserRouter as Router} from 'react-router-dom';
+
+import Home from '../../sections/Home/Home'
 import Editor from "../../sections/Editor/Editor";
+import NotFound from "../../components/NotFound";
 
 function App() {
   return (
-    <div className="App">
-      {/* <Home></Home> */}
-      <Editor></Editor>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" render={() => <Home/>}/>
+        <Route exact path="/editor" render={() => <Editor />}/>
+        <Route render={() => <NotFound />} />
+      </Switch>
+    </Router>
   );
 }
 
